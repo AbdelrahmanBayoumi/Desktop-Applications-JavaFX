50765979608c0d8f65026f3b6ea4d315db11d0fd
Abdelrahman Bayoumi <48678280+AbdelrahmanBayoumi@users.noreply.github.com>
2019-10-27 12:29:43 +0200

    00

--- Random-Number-Generator-JavaFX/dist/web-files/dtjava.js
+++ Random-Number-Generator-JavaFX/dist/web-files/dtjava.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006, 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2006, 2017, Oracle and/or its affiliates. All rights reserved.
  * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
  */
 
@@ -171,23 +171,22 @@ var dtjava = function() {
         var edge = false;
         var noActiveX = false;
         edge = (navigator.userAgent.match(/Edge/i) != null);
-        
+
         // If IE and Windows 8 or Windows 8.1 then check for Metro mode
         if(ie && navigator.userAgent.match(/Windows NT 6\.[23]/i) != null) {
             try {
                 // try to create a known ActiveX object
                 new ActiveXObject("htmlfile");
             } catch(e) {
-		// ActiveX is disabled or not supported. 
+		// ActiveX is disabled or not supported.
                 noActiveX = true;
-            } 
+            }
         }
 
         if(edge || noActiveX) {
             ie = false;
 	}
 
-	var noPluginWebBrowser = edge || chrome || noActiveX;
 
         //we are not required to detect everything and can leave values null as
         // long as we later treat them accordingly.
@@ -208,7 +207,7 @@ var dtjava = function() {
         // trim() is not supported by IE10 and before
         if(typeof String.prototype.trim !== 'function') {
            String.prototype.trim = function() {
-               return this.replace(/^\s+|\s+$/g, ''); 
+               return this.replace(/^\s+|\s+$/g, '');
            }
         }
 
@@ -247,7 +246,7 @@ var dtjava = function() {
                 if (m.indexOf("application/x-java-applet;version") != -1 && m.indexOf('=') != -1) {
                     var v = m.substring(m.indexOf('=') + 1);
                     // Use the existing version comparison mechanism to ensure that
-                    // the latest JRE is selected ( "versionA"<="VersionB" equals to 
+                    // the latest JRE is selected ( "versionA"<="VersionB" equals to
                     // versionCheck("versionA+","versionB") returns "true")
                     if(jre == null || versionCheck(jre + "+", v)){
 			jre = v;
@@ -263,7 +262,12 @@ var dtjava = function() {
                 }
             }
         }
-		
+
+       var firefox = (navigator.userAgent.match(/Firefox/i) != null);
+
+       var noPluginWebBrowser = edge || chrome || noActiveX || (firefox && (jre == null));
+
+
         return {haveDom:dom, wk:webkit, ie:ie, win:windows,
                 linux:linux, mac:mac, op: opera, chrome:chrome, edge:edge,
                 jre:jre, deploy:deploy, fx:fx, noPluginWebBrowser:noPluginWebBrowser,
@@ -272,17 +276,17 @@ var dtjava = function() {
 
    function showMessageBox() {
         var message = 'Java Plug-in is not supported by this browser. <a href="https://java.com/dt-redirect">More info</a>';
-        var mbStyle = 'background-color: #ffffce;text-align: left;border: solid 1px #f0c000; padding: 1.65em 1.65em .75em 0.5em; font-family: Helvetica, Arial, sans-serif; font-size: 75%; top:5;left:5;position:absolute; opacity:0.9; width:600px;';
+        var mbStyle = 'background-color: #ffffce;text-align: left;border: solid 1px #f0c000; padding: 1.65em 1.65em .75em 0.5em; font-family: Helvetica, Arial, sans-serif; font-size: 75%; bottom:0; left:0; right:0; position:fixed; margin:auto; opacity:0.9; width:400px;';
         var messageStyle = "border: .85px; margin:-2.2em 0 0.55em 2.5em;";
-
-        var messageBox = '<img src="https://java.com/js/alert_16.png"><div style="'+ messageStyle +'"><p>'+ message + '</p>';
+        var closeButtonStyle = "margin-left:10px;font-weight:bold;float:right;font-size:22px;line-height:20px;cursor:pointer;color:red;"
+        var messageBox = '<span style="'+ closeButtonStyle +'" onclick="this.parentElement.style.display=\'none\';">&times;</span><img src="https://java.com/js/alert_16.png"><div style="'+ messageStyle +'"><p>'+ message + '</p>';
 
 
         var divTag = document.createElement("div");
         divTag.id = "messagebox";
         divTag.setAttribute('style', mbStyle);
         divTag.innerHTML = messageBox;
-        document.body.appendChild(divTag);              
+        document.body.appendChild(divTag);
 
     }
     //partially derived from swfobject.js
@@ -390,7 +394,7 @@ var dtjava = function() {
             installNativePlugin();
         }
     }
-    
+
    function getAbsoluteUrl(jnlp){
         var absoluteUrl;
         if(isAbsoluteUrl(jnlp)) {
@@ -407,7 +411,7 @@ var dtjava = function() {
     function launchWithJnlpProtocol(jnlp) {
         document.location="jnlp:"+ getAbsoluteUrl(jnlp);
     }
-  
+
 
     function isAbsoluteUrl(url){
        var protocols = ["http://", "https://", "file://"];
@@ -2743,7 +2747,7 @@ var dtjava = function() {
             }
         }
         if (notNull(platform.jvmargs)) {
-            sparams["java_arguments"] = platform.jvmargs;
+            sparams["java_arguments"] = listToString(platform.jvmargs);
         }
 
         //prepare parameters first
